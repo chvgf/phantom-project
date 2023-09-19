152119e8bd6c41eeaa841445c5f5cafcebd32b37
chvgf <chvgf@naver.com>
2023-09-19 21:39:14 +0900

    팬텀디자인
    콘텐츠2 슬라이드

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 헤더 스크롤 시 백그라운드 컬러 번경
 const hEl = document.querySelector('header');
 
 window.addEventListener('scroll', function () {
@@ -5,18 +6,24 @@ window.addEventListener('scroll', function () {
 console.log(window.scrollY);
 
 if (window.scrollY > 100) {
-
-
-    gsap.to(hEl, 0.6, {
-    opacity: 0,
-    display: 'none'
-    });
+    hEl.classList.add('header-bg')
 } else {
-
-
-    gsap.to(hEl, 0.6, {
-    opacity: 1,
-    display: 'block'
-    });
+    hEl.classList.remove('header-bg')
 }
 });
+
+// CONTENT 1 애니메이션
+const conBoxEl = document.querySelectorAll('.content .box')
+
+window.addEventListener('scroll', function () {
+
+    // console.log(window.scrollY);
+    conBoxEl.forEach(function (conBoxEl, index) {
+        
+
+    if (window.scrollY > 500) {
+        conBoxEl.classList.add('show')
+    } else {
+        conBoxEl.classList.remove('show')
+    }
+    })});
